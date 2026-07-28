bcee4359b0d01351c744e18bdd1f42877272e55d
yourcoachywarneo127 <yourcoach.yw.arneo127@gmail.com>
2026-07-28 23:04:31 +0900

    Create results.js

---
+++ data/results.js
@@ -0,0 +1,12 @@
+/**
+ * 復学ロードマップのステップデータ
+ */
+
+const ROADMAP_STEPS = [
+    { step: 1, name: "回復の土台づくり期" },
+    { step: 2, name: "エネルギー回復期" },
+    { step: 3, name: "自信育成期" },
+    { step: 4, name: "一歩準備期" },
+    { step: 5, name: "復学目前期" },
+    { step: 6, name: "スタート目前期" }
+];
